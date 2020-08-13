1a8689430782ceb20171a13b495a73f4eb985d04
Janak <ruiajanak@gmail.com>
2020-08-13 21:49:29 +0530

    A folder for sharing logical (python) files

--- scripts/obstacle_avoidance.py
+++ scripts/obstacle_avoidance.py
@@ -7,6 +7,7 @@ pub = None
 
 def clbk_laser(msg):
 	n = len(msg.ranges)
+	#distribute total region (half a circle) into 3 parts
 	ranges = {
 		'right': min(msg.ranges[0:n//3]),
 		'front': min(msg.ranges[n//3:2*n//3]),
@@ -19,22 +20,26 @@ def take_action(regions):
 	msg = Twist()
 	linear_x = 0
 	angular_z = 0
-	state_description = ''
+	state_description = '' #for personal info
 
 	f= regions['front']
 	r = regions['right']
 	l = regions['left']
+	
+	#if u can go forward, go
 	if f>1:
 		state_description = 'case 1 - nothing'
 		linear_x = 0.6
 		angular_z = 0
 	else:
+		#else, if u can go right, turn right (with a little forward vel (just for fun))
 		if r>1:
 			state_description = 'right is free'
 			angular_z = -0.3
 			linear_x = 0.05	
 
-
+		#if not enough space on right, turn right faster and on the spot
+		#here, if u try to turn left, the robot might end up oscillating between right and left
 		else:
 			state_description = 'moving on the spot'
 			angular_z = -0.5
