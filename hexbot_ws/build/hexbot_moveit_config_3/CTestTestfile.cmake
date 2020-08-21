14f1e83f125deb818f44468c5de227e6a8851902
Janak <ruiajanak@gmail.com>
2020-08-21 15:43:42 +0530

    uploaded full bot with moveit

---
+++ hexbot_ws/build/hexbot_moveit_config_3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/janak/hexbot_ws/src/hexbot_moveit_config_3
+# Build directory: /home/janak/hexbot_ws/build/hexbot_moveit_config_3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
